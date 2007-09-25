b5ed86325a6656b736a1f7c9961485dbcb054a5f
cs186 <cs186@bda5494f-ab5d-eaf6-e90e-95e7cf522e9b>
2007-09-25 08:56:30 +0000

    install gist_private.h

--- hw/hw2/MyStuff/install.sh
+++ hw/hw2/MyStuff/install.sh
@@ -1,4 +1,4 @@
 #/bin/csh
 cp gistget.c ../postgresql-8.2.4/src/backend/access/gist/gistget.c
 cp geo_ops.c ../postgresql-8.2.4/src/backend/utils/adt/geo_ops.c
-
+cp gist_private.h ../postgresql-8.2.4/src/include/access/gist_private.h
