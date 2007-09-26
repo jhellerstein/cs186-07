cb0741aadbd1886a6340f99f41c96fdc7b57f83a
cs186 <cs186@bda5494f-ab5d-eaf6-e90e-95e7cf522e9b>
2007-09-26 05:11:07 +0000

    California data set, add to README and setup.sql

--- hw/hw2/MyStuff/setup.sql
+++ hw/hw2/MyStuff/setup.sql
@@ -1,3 +1,9 @@
-create table test(id integer, bounding_box box);
-copy test from 'randomboxes' delimiter ';' ;
-create index boxix on test using gist (bounding_box box_ops);
+create table test(id integer, the_box box);
+copy test from '/home/cc/cs186/fa07/coass/cs186-XX/Hw2/MyStuff/randomboxes' delimiter ';' ;
+create index ix1 on test using gist (the_box box_ops);
+create table test25 (id integer, the_box box);
+copy test25 from '/home/cc/cs186/fa07/class/cs186-XX/Hw2/MyStuff/randomboxes25' delimiter ';' ;
+create index ix2 on test25 using gist (the_box box_ops);
+create table cali(location box, name text);
+copy cali from  '/home/cc/cs186/fa07/class/cs186-XX/Hw2/MyStuff/cali.txt' delimiter '|';
+create index cali_ix on cali using gist(location box_ops);
