e66f3b5566a737030d92d65fd775ca3c5a378fc5
cs186 <cs186@bda5494f-ab5d-eaf6-e90e-95e7cf522e9b>
2007-09-26 18:09:44 +0000

    never call box_distance for mindist calculations, it uses box centers not edges.

--- hw/hw2/postgresql-8.2.4/src/backend/access/gist/gistproc.c
+++ hw/hw2/postgresql-8.2.4/src/backend/access/gist/gistproc.c
@@ -127,14 +127,7 @@ gist_box_mindistance(PG_FUNCTION_ARGS)
 	  d = get_float8_infinity();
 	  PG_RETURN_FLOAT8(d);
 	}
-	/*
-	 * if entry is not leaf, use box_mindist_internal, else use
-	 * box_distance_internal
-	 */
-  if (GIST_LEAF(entry))
-    d = box_distance_internal(DatumGetBoxP(entry->key), query);
-	else
-	  d = box_mindist_internal(DatumGetBoxP(entry->key), query);
+        d = box_mindist_internal(DatumGetBoxP(entry->key), query);
 	PG_RETURN_FLOAT8(d);
 }
 
